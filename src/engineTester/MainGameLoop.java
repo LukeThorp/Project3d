4a5621b396b41ba70a3f0a7f9abab843bf7e0f89
lothorp <thorp.luke@gmail.com>
2018-10-08 21:53:08 +0100

    turorial 2 - complete (VAOs and VBOs)

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,7 +2,11 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 	
@@ -10,10 +14,27 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				// left bottom triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				// right top triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
-			
+			renderer.prepare();
+
 			// game logic
-			// render
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 			
 		}
